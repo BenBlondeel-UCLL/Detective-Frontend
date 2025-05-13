5bded7459bef14711c2712fc2ef8434266a91e7d
simonDeschagt <simon.deschagt@student.ucll.be>
2025-05-13 16:42:42 +0200

    added post request

--- lib/feature/http_client.dart
+++ lib/feature/http_client.dart
@@ -6,7 +6,7 @@ class HttpClient{
 
   getHttp() async {
     final dio = Dio();
-    final response = await dio.get('http://127.0.0.1:8000');
+    final response = await dio.post('http://127.0.0.1:8000/analyse', queryParameters: {'text': 'Dit slechte zin.'});
     return response;
   }
 }
