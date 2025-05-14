ea4a2ec1dfedf330493001b6562b975880fa8d21
VolkertBrusche-UCLL <volkert.brusche@student.ucll.be>
2025-05-14 09:32:18 +0200

    Moved Home page code to a separate file

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
-import 'package:detective/features/analyse_button.dart';
-import 'package:detective/features/header.dart';
-import 'package:detective/features/input_field.dart';
-import 'package:detective/features/http_client.dart';
+import 'package:detective/pages/home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -36,29 +33,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          spacing: 30,
-          children: [
-            const Header(),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0, 30, 0, 0),
-              child: Column(
-                children: [InputField(), SizedBox(height: 20), AnalyseButton()],
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                final client = HttpClient();
-                final response = await client.getHttp();
-                print(response);
-              },
-              child: const Text('Press Me'),
-            ),
-          ],
-        ),
-      ),
+      body: Home()
     );
   }
 }
---
+++ lib/pages/home.dart
@@ -0,0 +1,36 @@
+import 'package:detective/features/analyse_button.dart';
+import 'package:detective/features/header.dart';
+import 'package:detective/features/input_field.dart';
+import 'package:detective/features/http_client.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        spacing: 30,
+        children: [
+          const Header(),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 30, 0, 0),
+            child: Column(
+              children: [InputField(), SizedBox(height: 20), AnalyseButton()],
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () async {
+              final client = HttpClient();
+              final response = await client.getHttp();
+              print(response);
+            },
+            child: const Text('Press Me'),
+          ),
+        ],
+      ),
+    );
+  }
+}
