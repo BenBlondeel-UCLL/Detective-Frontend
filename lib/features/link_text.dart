95a4de31f53b2ea9e6b137741137857af7af0666
VolkertBrusche-UCLL <volkert.brusche@student.ucll.be>
2025-06-11 15:48:50 +0200

    Fixed overflowing issue in link_text.dart

--- lib/features/link_text.dart
+++ lib/features/link_text.dart
@@ -9,30 +9,24 @@ class LinkText extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Expanded(
-          child: InkWell(
-            onTap: () async {
-              final url = Uri.parse(
-                this.url.startsWith('http') ? this.url : 'https://${this.url}',
-              );
-              if (await canLaunchUrl(url)) {
-                await launchUrl(url, mode: LaunchMode.externalApplication);
-              }
-            },
-            child: Text(
-              title,
-              style: TextStyle(
-                color: Colors.blue,
-                decoration: TextDecoration.underline,
-              ),
-              overflow: TextOverflow.ellipsis,
-              softWrap: false,
-            ),
-          ),
+    return InkWell(
+      onTap: () async {
+        final url = Uri.parse(
+          this.url.startsWith('http') ? this.url : 'https://${this.url}',
+        );
+        if (await canLaunchUrl(url)) {
+          await launchUrl(url, mode: LaunchMode.externalApplication);
+        }
+      },
+      child: Text(
+        title,
+        style: TextStyle(
+          color: Colors.blue,
+          decoration: TextDecoration.underline,
         ),
-      ],
+        overflow: TextOverflow.ellipsis,
+        softWrap: false,
+      ),
     );
   }
 }
